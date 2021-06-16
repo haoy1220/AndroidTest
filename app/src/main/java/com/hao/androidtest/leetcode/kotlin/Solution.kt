f52fb2855e5e4d731833c26a85e75c4a7b380763
haoy1220 <haoaying@outlook.com>
2021-06-16 10:10:52 +0800

    [add]

--- app/src/main/java/com/hao/androidtest/leetcode/kotlin/Solution.kt
+++ app/src/main/java/com/hao/androidtest/leetcode/kotlin/Solution.kt
@@ -1,6 +1,7 @@
 package com.hao.androidtest.leetcode.kotlin
 
 class Solution {
+    //下一个排序
     fun nextPermutation(nums: IntArray): Unit {
         var i = nums.size - 2;
         while (i >= 0 && nums[i] >= nums[i + 1]) {
@@ -30,5 +31,31 @@ class Solution {
         var temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
+    }
+
+    //在排序数组中查找元素的第一个和最后一个位置
+    fun searchRange(nums: IntArray, target: Int): IntArray {
+        var leftIndex = binarySearch(nums, target, true);
+        var rightIndex = binarySearch(nums, target, false) - 1;
+        if (leftIndex <= rightIndex && rightIndex < nums.size && nums[leftIndex] == target && nums[rightIndex] == target) {
+            return intArrayOf(leftIndex, rightIndex);
+        }
+        return intArrayOf(-1, -1);
+    }
+
+    private fun binarySearch(nums: IntArray, target: Int, lower: Boolean): Int {
+        var left = 0;
+        var right = nums.size - 1;
+        var ans = nums.size;
+        while (left <= right) {
+            val mid = (left + right) / 2;
+            if (nums[mid] > target || (lower && nums[mid] >= target)) {
+                right = mid - 1;
+                ans = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
     }
 }
